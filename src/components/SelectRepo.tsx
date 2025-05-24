6a704775ce1e625de0e63a8fd1e561dbd9060afd
Josh Dales <git@joshdales.dev>
2025-05-24 15:00:56 -0400

    More configuring the file picker

--- src/components/SelectRepo.tsx
+++ src/components/SelectRepo.tsx
@@ -43,6 +43,8 @@ export default function SelectRepo() {
 				id="newRepo"
 				title="Repo Directory"
 				canChooseDirectories
+				storeValue
+				allowMultipleSelection={false}
 				canChooseFiles={false}
 				defaultValue={value ? [value] : undefined}
 				autoFocus
