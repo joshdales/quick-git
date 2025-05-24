3bbe8088b5141fced0758576533346cf38dfb021
Josh Dales <git@joshdales.dev>
2025-05-24 15:22:36 -0400

    Update all the messages again

--- src/components/GitStatusEmpty.tsx
+++ src/components/GitStatusEmpty.tsx
@@ -10,38 +10,32 @@ interface Props {
 export function GitStatusEmpty({ branch, repo }: Props) {
 	const title = useMemo(() => {
 		if (repo && branch) {
-			const title = `On branch ${branch?.name}`
-			const { ahead, behind, upstream } = branch
-			if (upstream && (ahead || behind)) {
-				if (ahead && behind) {
-					return (
-						title +
-						`. Ahead of '${upstream}' by ${ahead}, and behind by ${behind} commits`
-					)
-				}
-
-				if (ahead && !behind) {
-					return title + `, ahead of '${upstream}' by ${ahead} commits.`
-				}
-
-				if (!ahead && behind) {
-					return title + `, behind '${upstream}' by ${behind} commits.`
-				}
-			}
-
-			return title
+			return `On branch ${branch?.name}`
 		}
 
 		return "Please select a repo"
 	}, [branch, repo])
 
 	const description = useMemo(() => {
-		if (!repo) {
+		if (!repo || !branch) {
 			return
 		}
 
-		return `Nothing to commit, working tree clean`
-	}, [repo])
+		const { ahead, behind, upstream } = branch
+		if (upstream && (ahead || behind)) {
+			if (ahead && behind) {
+				return `Ahead of '${upstream}' by ${ahead}, and behind by ${behind} commits`
+			}
+
+			if (ahead && !behind) {
+				return `Ahead of '${upstream}' by ${ahead} commits.`
+			}
+
+			if (!ahead && behind) {
+				return `Behind '${upstream}' by ${behind} commits.`
+			}
+		}
+	}, [branch, repo])
 
 	return <List.EmptyView title={title} description={description} />
 }
