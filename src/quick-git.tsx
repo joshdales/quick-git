17fffff6d5560e1f1d61bb6f684a8a02578a9cea
Josh Dales <git@joshdales.dev>
2025-05-24 07:46:16 -0400

    simpler naming

--- src/quick-git.tsx
+++ src/quick-git.tsx
@@ -2,7 +2,7 @@ import { useLocalStorage } from "@raycast/utils"
 import { GitStatus } from "./components/GitStatus.js"
 
 export default function Command() {
-	const { value: repo, isLoading } = useLocalStorage<string>("repo")
+	const { value, isLoading } = useLocalStorage<string>("repo")
 
-	return <GitStatus repo={repo} isLoading={isLoading} />
+	return <GitStatus repo={value} isLoading={isLoading} />
 }
