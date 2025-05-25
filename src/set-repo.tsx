9a5d43da4c45db41c5049376ed77fe2fc7879cf7
Josh Dales <git@joshdales.dev>
2025-05-25 16:25:12 -0400

    Remove unused context

--- src/set-repo.tsx
+++ src/set-repo.tsx
@@ -31,7 +31,6 @@ export default function Command() {
 					launchCommand({
 						name: "quick-git",
 						type: LaunchType.UserInitiated,
-						context: { foo: "bar" },
 					})
 				})
 				.catch((error) => {
