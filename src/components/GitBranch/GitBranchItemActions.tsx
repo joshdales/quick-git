1f0de1cafaee27f091d9ab946a2fe913d3b42606
Josh Dales <git@joshdales.dev>
2025-05-25 17:06:42 -0400

    Include branch name hwne switching

--- src/components/GitBranch/GitBranchItemActions.tsx
+++ src/components/GitBranch/GitBranchItemActions.tsx
@@ -23,7 +23,7 @@ export function GitBranchItemActions({
 		onData: () => {
 			checkBranches()
 			checkStatus()
-			showToast({ title: "Switched branch" })
+			showToast({ title: `Switched branch to ${branch}` })
 		},
 		onError: (error) => {
 			showFailureToast(error, { title: `Could not switch to ${branch}` })
