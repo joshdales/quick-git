76513b71fa90a23b7c5d34087082d5bdc2c0e217
Josh Dales <git@joshdales.dev>
2025-06-11 22:43:45 -0400

    Update submodle parsing

--- src/utils/submodule.ts
+++ src/utils/submodule.ts
@@ -7,11 +7,10 @@ export interface SubmoduleStatus {
 
 /**
  * Parse submodule status from poreclain version 2.
- * @param {string} sub A 4 character field describing the submodule state
+ * @param sub A 4 character field describing the submodule state
  */
 export function parseSubmodule(sub: string): SubmoduleStatus {
-  const isSubmodule = sub.charAt(0) === "S";
-  if (!isSubmodule) {
+  if (!sub || sub === "N...") {
     return {
       isSubmodule: false,
       commitHasChanged: false,
@@ -19,14 +18,11 @@ export function parseSubmodule(sub: string): SubmoduleStatus {
       hasUntrackedChanges: false,
     };
   }
-  const commitHasChanged = sub.charAt(1) === "C";
-  const hasTrackedChanges = sub.charAt(2) === "M";
-  const hasUntrackedChanges = sub.charAt(3) === "U";
 
   return {
-    isSubmodule,
-    commitHasChanged,
-    hasTrackedChanges,
-    hasUntrackedChanges,
+    isSubmodule: sub.charAt(0) === "S",
+    commitHasChanged: sub.charAt(1) === "C",
+    hasTrackedChanges: sub.charAt(2) === "M",
+    hasUntrackedChanges: sub.charAt(3) === "U",
   };
 }
