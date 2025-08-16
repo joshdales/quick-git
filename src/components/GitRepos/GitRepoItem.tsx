063b35b625e7e0c58c615e0316369d63364ad785
Josh Dales <git@joshdales.dev>
2025-08-16 18:02:19 -0400

    Add action to go to git status

--- src/components/GitRepos/GitRepoItem.tsx
+++ src/components/GitRepos/GitRepoItem.tsx
@@ -4,6 +4,7 @@ import { RepoDir } from "../../utils/repos.js";
 import { SelectCurrentRepo } from "../actions/ChangeCurrentRepo.js";
 import { ChooseSpecificRepo } from "../actions/ChooseSpecificRepo.js";
 import { OpenDirectory } from "../actions/OpenDirectory.js";
+import { CheckStatus } from "../actions/CheckStatus.js";
 
 interface Props {
   repoDir: RepoDir;
@@ -22,15 +23,24 @@ export function GitRepoItem({ repoDir, isSelected, changeRepo }: Props) {
     changeRepo(repoDir);
   }, [changeRepo, repoDir]);
 
-  const actions = useMemo(() => {
-    return (
-      <ActionPanel>
-        {isSelected ? null : <SelectCurrentRepo selectRepo={selectRepo} />}
-        <OpenDirectory path={repoDir.id} />
-        <ChooseSpecificRepo />
-      </ActionPanel>
-    );
-  }, [isSelected, repoDir.id, selectRepo]);
+  const selectedActions = useMemo(() => {
+    if (isSelected) {
+      return <CheckStatus />;
+    }
+    return <SelectCurrentRepo selectRepo={selectRepo} />;
+  }, [isSelected, selectRepo]);
 
-  return <List.Item title={repoDir.label} accessories={accessories} actions={actions} />;
+  return (
+    <List.Item
+      title={repoDir.label}
+      accessories={accessories}
+      actions={
+        <ActionPanel>
+          {selectedActions}
+          <OpenDirectory path={repoDir.id} />
+          <ChooseSpecificRepo />
+        </ActionPanel>
+      }
+    />
+  );
 }
