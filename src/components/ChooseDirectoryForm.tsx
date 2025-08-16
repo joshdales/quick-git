fb06b2326e3e2a144e16f092a4818b6cee315df8
Josh Dales <git@joshdales.dev>
2025-08-16 14:39:39 -0400

    Extract choosing directory form to component

---
+++ src/components/ChooseDirectoryForm.tsx
@@ -0,0 +1,65 @@
+import { Action, ActionPanel, Form, Icon, launchCommand, LaunchType, showToast, Toast } from "@raycast/api";
+import { FormValidation, showFailureToast, useForm } from "@raycast/utils";
+import { useRepoStorage } from "../hooks/useRepo.js";
+import { OpenPreferences } from "./actions/OpenPreferences.js";
+
+export function ChooseDirectoryForm() {
+  const repo = useRepoStorage();
+
+  const { handleSubmit, itemProps } = useForm({
+    onSubmit({ newRepo }: { newRepo: string[] }) {
+      repo
+        .setValue(newRepo[0])
+        .then(() => {
+          showToast({
+            style: Toast.Style.Success,
+            title: "Repo set",
+            message: `${newRepo[0]}`,
+          });
+
+          launchCommand({
+            name: "quick-git",
+            type: LaunchType.UserInitiated,
+          }).catch((error) => {
+            showFailureToast(error, {
+              title: "Could not launch the Quick Git command",
+            });
+          });
+        })
+        .catch((error) => {
+          showFailureToast(error, {
+            title: "Could not set this as your selected repo",
+          });
+        });
+    },
+    validation: {
+      newRepo: FormValidation.Required,
+    },
+  });
+
+  return (
+    <Form
+      navigationTitle="Select Git Repo"
+      isLoading={repo.isLoading}
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Set Repo" onSubmit={handleSubmit} icon={Icon.Checkmark} />
+          <Action.SubmitForm title="Unset Repo" onSubmit={repo.removeValue} icon={Icon.Xmark} />
+          <OpenPreferences />
+        </ActionPanel>
+      }
+    >
+      <Form.FilePicker
+        id="newRepo"
+        title="Repo Directory"
+        canChooseDirectories
+        storeValue
+        allowMultipleSelection={false}
+        canChooseFiles={false}
+        defaultValue={repo.value ? [repo.value] : undefined}
+        autoFocus
+        error={itemProps.newRepo.error}
+      />
+    </Form>
+  );
+}
