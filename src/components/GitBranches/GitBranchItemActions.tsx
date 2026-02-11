194993898af31d142c5143c5679f01d8187d2c67
Josh Dales <git@joshdales.dev>
2026-02-11 14:44:30 -0800

    render the delete worktree action

--- src/components/GitBranches/GitBranchItemActions.tsx
+++ src/components/GitBranches/GitBranchItemActions.tsx
@@ -2,10 +2,14 @@ import { useMemo } from "react";
 import { ActionPanel } from "@raycast/api";
 import { SwitchToBranch } from "../actions/SwitchToBranch.js";
 import { DeleteBranch } from "../actions/DeleteBranch.js";
+import { DeleteWorktree } from "../actions/DeleteWorktree.js";
 import { CreateNewBranch } from "../actions/CreateNewBranch.js";
 import { SwitchToLastBranch } from "../actions/SwitchToLastBranch.js";
 import { SwitchToWorkTree } from "../actions/SwitchToWorktree.js";
 import { CreateNewWorkTree } from "../actions/CreateNewWorktree.js";
+import { useCachedPromise } from "@raycast/utils";
+import { findWorktreeDir } from "../../utils/worktrees.js";
+import { useRepo } from "../../hooks/useRepo.js";
 
 interface Props {
   branch: string;
@@ -16,9 +20,19 @@ interface Props {
 }
 
 export function GitBranchItemActions({ branch, isCurrentBranch, isWorktree, checkBranches, updateRepo }: Props) {
+  const repo = useRepo();
+  const worktreeDir = useCachedPromise(findWorktreeDir, [repo ?? "", branch], {
+    execute: !!repo,
+  });
+
   const actions = useMemo(() => {
     if (isWorktree) {
-      return <SwitchToWorkTree worktree={branch} updateRepo={updateRepo} />;
+      return (
+        <>
+          <SwitchToWorkTree worktree={branch} updateRepo={updateRepo} />
+          <DeleteWorktree worktreeName={branch} worktreePath={worktreeDir.data ?? ""} checkBranches={checkBranches} />
+        </>
+      );
     }
 
     if (!isCurrentBranch) {
@@ -31,7 +45,7 @@ export function GitBranchItemActions({ branch, isCurrentBranch, isWorktree, chec
     }
 
     return null;
-  }, [branch, checkBranches, isCurrentBranch, isWorktree]);
+  }, [branch, checkBranches, isCurrentBranch, isWorktree, worktreeDir.data]);
 
   return (
     <ActionPanel>
