ea1422ed6c125098aedd99f7f321166feffe469b
Josh Dales <git@joshdales.dev>
2026-06-14 20:45:04 -0400

    Update repo context hooks with error checks

--- src/hooks/useRepo.ts
+++ src/hooks/useRepo.ts
@@ -1,10 +1,14 @@
 import { useLocalStorage } from "@raycast/utils";
 import { createContext, useContext } from "react";
 
-export type SelectedRepo = ReturnType<typeof useLocalStorage<string>>;
+type RawSelectedRepo = ReturnType<typeof useLocalStorage<string>>;
+export type SelectedRepo = Pick<RawSelectedRepo, "isLoading" | "removeValue" | "setValue"> & {
+  value: string;
+};
 
 export const RepoContext = createContext<SelectedRepo>({
-  value: undefined,
+  // null is used to indicate that context has not been initialized
+  value: null as unknown as string,
   setValue: () => {
     throw Error("Cannot set repo value: RepoContext was not initialized");
   },
@@ -14,14 +18,24 @@ export const RepoContext = createContext<SelectedRepo>({
   isLoading: false,
 });
 
-export function useSelectedRepoStorage(): SelectedRepo {
+export function useSelectedRepoStorage(): RawSelectedRepo {
   return useLocalStorage<string>("selectedRepo");
 }
 
 export function useRepo(): string {
-  return useContext(RepoContext).value ?? "";
+  const { value } = useContext(RepoContext);
+  if (value === null) {
+    throw new Error("Cannot get repo value: RepoContext was not initialized");
+  }
+
+  return value;
 }
 
 export function useSelectedRepo(): SelectedRepo {
-  return useContext(RepoContext);
+  const repo = useContext(RepoContext);
+  if (repo.value === null) {
+    throw new Error("Cannot get repo data: RepoContext was not initialized");
+  }
+
+  return repo;
 }
